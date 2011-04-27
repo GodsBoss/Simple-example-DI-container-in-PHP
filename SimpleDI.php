ac9527b8f2e1df018e578971235df86b933803b5
GodsBoss <godsboss@gmx.de>
2011-04-27 23:14:35 +0200

    Get and retrieve parameters.

--- SimpleDI.php
+++ SimpleDI.php
@@ -1,3 +1,11 @@
 <?php
 
-class SimpleDI{}
+class SimpleDI{
+	private $params = array();
+
+	public function setParameter($name, $value){
+		$this->params[$name] = $value;
+		return $this;}
+
+	public function get($name){
+		return $this->params[$name];}}
--- SimpleDITest.php
+++ SimpleDITest.php
@@ -4,4 +4,9 @@ require_once('SimpleDI.php');
 
 class SimpleDITest extends PHPUnit_Framework_TestCase{
 	public function test_construction(){
-		$di = new SimpleDI();}}
+		$di = new SimpleDI();}
+
+	public function test_parameter(){
+		$di = new SimpleDI();
+		$di->setParameter('foo', 'bar');
+		$this->assertEquals('bar', $di->get('foo'));}}
