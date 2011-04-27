e1710ecb78950a35a4a5562d6488c4849c89fd8d
GodsBoss <godsboss@gmx.de>
2011-04-27 23:55:24 +0200

    Added test for fluent interface for setting parameters.

--- SimpleDITest.php
+++ SimpleDITest.php
@@ -17,6 +17,10 @@ class SimpleDITest extends PHPUnit_Framework_TestCase{
 		$di->setParameter('foo', 'bar');
 		$this->assertEquals('bar', $di->get('foo'));}
 
+	public function test_returning_container_after_setting_parameter(){
+		$di = new SimpleDI();
+		$this->assertTrue($di === $di->setParameter('foo', 'bar'));}
+
 	public function test_out_of_bounds_exception_for_invalid_parameters(){
 		$di = new SimpleDI();
 		$this->setExpectedException('OutOfBoundsException');
